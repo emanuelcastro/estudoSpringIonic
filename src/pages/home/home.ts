c21c3bd769b441e9d64c424ed5ec015d0e4089bb
Emanuel Castro <emanuel.castro@gmail.com>
2018-12-30 11:25:59 -0300

    Commit aula 119

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -9,7 +11,14 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
+
+  constructor(public navCtrl: NavController, 
+              public menu: MenuController,
+              public auth: AuthService) {
 
   }
 
@@ -22,7 +31,12 @@ export class HomePage {
   }
 
   login(){
-      this.navCtrl.setRoot('CategoriasPage');
+      this.auth.authenticate(this.creds)
+        .subscribe(response => {
+          this.auth.successfullLogin(response.headers.get('Authorization'));
+          this.navCtrl.setRoot('CategoriasPage');
+        },
+        error => {});
   }
 
 }
