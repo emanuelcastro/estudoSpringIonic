def6c4be13a35fc60d8157eeccc4330db6fd1658
Emanuel Castro <emanuel.castro@gmail.com>
2018-12-31 10:53:29 -0300

    Commit ate a aula 132

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -30,6 +30,15 @@ export class HomePage {
     this.menu.swipeEnable(true);
   }
 
+  ionViewDidEnter() {
+    this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfullLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});  
+  }
+
   login(){
       this.auth.authenticate(this.creds)
         .subscribe(response => {
@@ -37,6 +46,10 @@ export class HomePage {
           this.navCtrl.setRoot('CategoriasPage');
         },
         error => {});
+  }
+
+  signup() {
+    this.navCtrl.push('SignupPage');
   }
 
 }
