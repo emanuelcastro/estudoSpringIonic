1726500b2ff2d53441aebc36fbdbb0a79b9886db
Emanuel Castro <emanuel.castro@gmail.com>
2019-01-14 16:01:17 -0300

    commit 14/01

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -11,25 +11,40 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+
+  page: number = 0;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              public produtoService: ProdutoService) {
+              public produtoService: ProdutoService,
+              public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
-    let categoria_id = this.navParams.get('categoria_id');
-    this.produtoService.findByCategoria(categoria_id)
-      .subscribe(response => 
-        {
-          this.items = response['content'];
-          this.loadImageUrls();
-        }, error => {})
+    this.loadData();
   };
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
+  loadData(){
+    let categoria_id = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
+      .subscribe(response => 
+        {
+          let start = this.items.length;
+          this.items = this.items.concat(response['content']);
+          let end = this.items.length - 1;
+          loader.dismiss();
+          console.log(this.page);
+          console.log(this.items);
+          this.loadImageUrls(start, end);
+        }, error => {
+          loader.dismiss();
+        })
+  }
+
+  loadImageUrls(start: number, end: number) {
+    for (var i=start; i<=end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -41,6 +56,30 @@ export class ProdutosPage {
 
   showDetail(produto_id : string) {
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: 'Aguarde...'
+    });
+  
+    loader.present();
+    return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,12 +4,14 @@ import { API_CONFIG } from "../../config/api.config";
 import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
 
 @Injectable()
 export class ClienteService {
 
     constructor(public http: HttpClient,
-                public storage: StorageService){    
+                public storage: StorageService,
+                public imageUtilService: ImageUtilService){    
     }
 
     findById(id: string) {
@@ -29,6 +31,20 @@ export class ClienteService {
         return this.http.post(
             `${API_CONFIG.baseUrl}/clientes`, 
             obj,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
+    }
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`, 
+            formData,
             { 
                 observe: 'response', 
                 responseType: 'text'
